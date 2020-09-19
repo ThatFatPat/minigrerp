fa3ed4f9c7d63855615510efc8cf8feccbc05c10
ThatFatPat <idoshav@gmail.com>
2020-09-19 21:26:35 +0300

    Read file; Improved config parsing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args);
 
-    println!("Searching for {}\nIn file {}", query, filename);
+    println!(
+        "Searching for {}\nIn file {}",
+        config.query, config.filename
+    );
+
+    let contents = fs::read_to_string(&config.filename)
+        .expect(&format!("Could not read file {}", config.filename)[..]);
+
+    println!("With text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+impl Config {
+    fn new(args: &[String]) -> Config {
+        Config {
+            query: args[1].clone(),
+            filename: args[2].clone(),
+        }
+    }
 }
