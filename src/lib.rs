903dfc8fe680a0931dcccb40e53495a1907b1454
ThatFatPat <idoshav@gmail.com>
2020-09-20 08:48:43 +0300

    Remove print

--- src/lib.rs
+++ src/lib.rs
@@ -20,7 +20,6 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(&config.filename)?;
 
     for line in search(&config.query, &contents) {
-        //println!("{}", line.line);
         colored_print(&line);
     }
     Ok(())
